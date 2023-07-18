d45fefcb8cb7e3dc3160732c71a94f2287a64fc4
Juram Lee <juram2@naver.com>
2023-07-18 12:40:53 +0900

    :sparkles: Update: feed detail page CSS 1차 완성

--- news/src/App.tsx
+++ news/src/App.tsx
@@ -1,12 +1,18 @@
 import 'tailwindcss/tailwind.css';
+import DetailInfo from './Pages/Detail/Components/info';
+import Comments from './Pages/Detail/Components/comments';
+import Detail from './Pages/Detail/detail';
 
 function App() {
   return (
-    <div className='w-full bg-slate-50 flex justify-center items-center'>
-      <div className='w-10/12 max-w-screen-xl border border-gray-700 p-20'>
-        TEST
+    <>
+      {/* <div className='w-full bg-slate-50 flex flex-col justify-center items-center'>
+        <div className='w-10/12 max-w-screen-xl border border-gray-700 p-20'></div>
+        <DetailInfo />
       </div>
-    </div>
+      <Comments /> */}
+      <Detail />
+    </>
   );
 }
 
