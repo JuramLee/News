afc0be9729cdf728f0ebda45b0801d4b5018753c
Juram Lee <juram2@naver.com>
2023-07-21 00:30:51 +0900

    :sparkles: Feat: Jotai 사용 시도

--- news/src/Pages/Main/main.tsx
+++ news/src/Pages/Main/main.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ListItem from './Components/listItem';
 import { Axios } from '../../Api/@core';
+import { atom, useAtom } from 'jotai';
+
+export const newsAtom = atom<any>('');
 
 const MainFeed: React.FC = () => {
-  // const [newsData, setNewsData] = useState('');
-  const dataTest = Axios.get('news/1.json').then((data) => console.log(data));
+  const [newsData, setNewsData] = useAtom(newsAtom);
+  useEffect(() => {
+    const res = Axios.get('news/1.json').then((data) => setNewsData(data.data));
+    console.log(newsData);
+  }, []);
 
-  console.log('@@', dataTest);
   return (
     <div className='flex flex-col w-full p-32 pl-10'>
       <ListItem />
